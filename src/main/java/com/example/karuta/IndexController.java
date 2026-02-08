ce0dcb8584f9ed5c4807a992665d49e2da0fadc2
macminim1 <macminim1@localhost>
2026-02-08 23:08:25 +0900

    1

--- src/main/java/com/example/karuta/IndexController.java
+++ src/main/java/com/example/karuta/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 /*
- * BR2 brtest２brTest　BR2MOD br2add1
+ * BR2
  */
 @Controller
 public class IndexController {
